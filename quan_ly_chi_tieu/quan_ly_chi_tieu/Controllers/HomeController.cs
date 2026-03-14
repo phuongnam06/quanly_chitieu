afd5c63d7fa6c4b09ae0f4cf142b4636977759cc
phuongnam <namdzcuti123@gmail.com>
2026-03-14 19:33:28 +0700

    phuongnam

---
+++ quan_ly_chi_tieu/quan_ly_chi_tieu/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using quan_ly_chi_tieu.Models;
+
+namespace quan_ly_chi_tieu.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ quan_ly_chi_tieu/quan_ly_chi_tieu/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace quan_ly_chi_tieu.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
